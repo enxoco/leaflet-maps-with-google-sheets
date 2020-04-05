e6e669e71aa09903f2a9692498312e9df24bcec1
Mike Conrad <34722899+enxoco@users.noreply.github.com>
2020-04-04 20:02:32 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1c38pRsXnSmc0kPLkJUVhDVBY-q5Pq778EZoeC7CDJxI/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1c38pRsXnSmc0kPLkJUVhDVBY-q5Pq778EZoeC7CDJxI/edit#gid=0';
